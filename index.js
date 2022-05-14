53b04df767311bff0baa652fa7ec5c6c323b3ebb
Angel Santos <iiisantosjangel@gmail.com>
2022-05-14 11:07:39 -0700

    worked on lib

--- index.js
+++ index.js
@@ -2,4 +2,24 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 // import classes
-const Employee = requi
+const Employee = require("./lib/employee");
+const Engineer =  require("./lib/Engineer");
+const intern = require("./lib/intern");
+const Manager = require("./lib/Manager");
+
+//Import HTML templates
+const addManagerCard = require(".src/card-manager");
+const addEngineerCard = require("./lib/card-engineer");
+ const addIternCard = require("./lib/card")
+
+//prompt manager questions
+const addManager = {
+    name:"role",
+    type:"confirm",
+    message: "Welcome to Team Profile Generator, Would you like to begin?",
+},
+{
+    name:"name",
+    type:"input",
+    message: "Please enter Manager's name:"
+},
