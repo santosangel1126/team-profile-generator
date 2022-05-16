4c439220040c103e25ee3e7e2b89c075d4259e70
Angel Santos <iiisantosjangel@gmail.com>
2022-05-15 21:43:42 -0700

    finished card-engineer

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const addManagerCard = require(".src/card-manager");
 const addEngineerCard = require("./src/card-engineer");
  const addIternCard = require("./src/card-intern");
  const wrapProfileCards = require("./src/card-wrapper");
+const { type } = require("os");
+const { profile } = require("console");
+const Intern = require("./lib/intern");
 
  const Team = [];
 
@@ -64,7 +67,7 @@ const addManager = [
     message: "what would you like to do next?"
 }
 ];
-
+// prompt engineer Questions
  const addEngineer = [
      {
          name: "name",
@@ -80,10 +83,131 @@ const addManager = [
                  return true;
              } else {
                  console.log("Please enter engineer ID");
+                 return false;
              }
          },
      },
-     
- ]
+     {
+         name:"email",
+         type:"input",
+         message: "Please enter email address",
+     },
+     {
+         name:"github",
+         type:"input",
+         message::"Please enter engineers github (Required)",
+         validate: (engGit) => {
+             if (engGit) {
+                 return true;
+             } else {
+                 console.log("Please enter Github engineers");
+                 return false;
+             }
+         },
+     },
+     {
+         name:"followUp",
+         type: "list"
+         choices:["Add Engineer","Add Intern","All done"],
+         message:"What would you like to do next?",
+     },
+ ];
+//prompt Intern Questions 
+const addIntern = [
+    {
+        name:"name",
+        type:"list",
+        choices:["Add Engineer","Add Intern","All done"],
+        message: "what would you like to do next?",
+    },
+    {
+        name:"School",
+        type:"input",
+        message:" Please enter school attended: (Required",
+        validate: (internSchool) => {
+            if (internSchool) {
+                return true;
+            } else {
+                console.log("Please enter school attended!");
+                return false;
+            }
+        },
+    },
+    {
+        name:"followUp",
+        type:"list",
+        choices:["Add Engineer","Add Intern","All done"],
+        message: " what would you like to do next?",
+    },
+];
+// Start initial questions
+questions(addManager);
+
+//cycle through questions when members are added 
+function questions (questionArr) {
+    inquirer
+    .prompt(questionArr)
+    .then(member) => {
+        Team.push(member);
+        if (member.followUp === "Add Engineer"){
+           questions(addEngineer);
+        } else if (member.followUp === "Add Intern") {
+            questions(addIntern);
+        } else {
+            createProfiles(team);
+        }
+    }
+    .catch((err) => console.log(err));
+  }
 
 
+function createProfiles(team) {
+    const profiles = team.map((member) => {
+        const {name,id,email} = member;
+        // adding manager
+        if (member.hasOwnProperty("github")) {
+            const {officeNumber} = member;
+            return new Manager(name, id, officeNumber); 
+        }
+       //adding engineer github required 
+       if (member.hasOwnProperty("github")) {
+           const {github} =member;
+           return new Engineer(name,id,email,github);
+       }
+       // adding intern  school required 
+       if(member.hasOwnProperty("school")) {
+           const {school} = member;
+           return new Intern(name,id,email,school);
+       }
+    });
+    //generate Html from employees progfiles 
+    generateHtml(profiles);
+}
+
+function generateHtml(profiles){
+    let profileCards ="";
+    profiles.forEach(profile => {
+        if (profile instanceof Manager){
+            const card = addManagerCard(profile);
+            profileCards += card;
+        } else if (profile instanceof Engineer) {
+            const card = addEngineerCard(profile);
+            profileCards += card;
+        } else if (profile instanceof Intern) {
+            const card = addIternCard(profile);
+            profileCards += card;
+        }
+    });
+
+    const newHtml = wrapProfileCards(profileCards);
+
+    writeHTML(newHtml);
+}
+ // function to write final HTML document in dist folder 
+ function writeHtml(newHtml);{
+     fs.writeFile("./dist/team-profile.html",newHtml, (err)=>{
+         if (err) throw err;
+         console.log(" HTML document sucessfully created in the /dist folder");
+     });
+ }       
+ 
